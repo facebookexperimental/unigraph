1390b1ed3474bb649a2f83a36301eba4a8ef3f93
Aaron Abrams <dabramov@meta.com>
2025-08-15 16:22:39 -0700

    MCPTieredJSArrowsWithMetricsReq
    Summary: MCP RPC call that returns arrows with tiered metrics
    
    Reviewed By: vladtsf
    
    Differential Revision: D80368048
    
    fbshipit-source-id: 4cf7fd6cc425f920384ac3856f7b51a1ac91e07e

--- u-be/unigraph_core/src/lib.rs
+++ u-be/unigraph_core/src/lib.rs
@@ -20,6 +20,7 @@ pub use types::NodeIDX;
 pub use types::SerializationFormat;
 pub use types::array_graph::ArrayGraph;
 pub use types::array_graph::ArrayGraphDynamicEdge;
+pub use types::array_graph::Arrow;
 pub use types::array_graph::array_graph_debug_utils::ArrayGraphDebugUtils;
 pub use types::array_graph::array_graph_nodes::ArrayGraphNodes;
 pub use types::array_graph::array_graph_nodes::GraphSide;
