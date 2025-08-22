fc95b2cbfd31ca88c7b43d4e38658e0cfc0e3212
Aaron Abrams <dabramov@meta.com>
2025-08-22 15:41:01 -0700

    ArrayGraphSerializable fetching
    Reviewed By: vladtsf
    
    Differential Revision: D80746802
    
    fbshipit-source-id: 4169d115684e86cc3fd455d5b77c589f131f6e73

--- u-be/unigraph_storage/src/manifest.rs
+++ u-be/unigraph_storage/src/manifest.rs
@@ -41,6 +41,40 @@ pub struct ManifestBlobs {
     pub entry_points: Vec<BlobID>,
 }
 
+impl ManifestBlobs {
+    pub fn get_all_blob_ids(&self) -> Vec<BlobID> {
+        let Self {
+            node_names,
+            node_names_offsets,
+            directed,
+            directed_offsets,
+            tagged,
+            dynamic,
+            metrics,
+            tag_sets,
+            traversal_config,
+            entry_points,
+        } = self;
+
+        [
+            node_names,
+            node_names_offsets,
+            directed,
+            directed_offsets,
+            tagged,
+            dynamic,
+            metrics,
+            tag_sets,
+            traversal_config,
+            entry_points,
+        ]
+        .into_iter()
+        .flatten()
+        .cloned()
+        .collect()
+    }
+}
+
 impl From<String> for BlobID {
     fn from(s: String) -> Self {
         BlobID(s)
