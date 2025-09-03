362db9f1716da08bcb747d505044262d2d349d2f
Aaron Abrams <dabramov@meta.com>
2025-09-02 18:33:12 -0700

    capacity overflow crash comment
    Reviewed By: vladtsf
    
    Differential Revision: D81358928
    
    fbshipit-source-id: 07e920a57b3d415cd6291a3981d746641c2a514e

--- u-be/unigraph_core/src/types/array_graph/offset_graph/shortest_path.rs
+++ u-be/unigraph_core/src/types/array_graph/offset_graph/shortest_path.rs
@@ -71,6 +71,27 @@ pub fn shortest_path(
         let mut current = needle;
 
         while let Some(&parent) = parents.get(&current) {
+            // NOTE: there is a capacity overflow error that happens sometimes with
+            // the stack that looks like:
+            //             hook.js:608 panicked at u-be/unigraph_core/src/types/array_graph/offset_graph/shortest_path.rs:74:18:
+            // capacity overflow
+            // Stack:
+            // Error
+            //     at imports.wbg.__wbg_new_8a6f238a6ece86ea (http://localhost:3000/?graph_settings=KLUv_QBodQUAAowiGIBtAwDCJ1H2OBbEHCX8K8A-TBmPJMyUBllVqfvY6X3_x_BZUv9b1yxqWGzHb7HfUx_kZ0102Gj2sWc1U9axL7ffttmqSm2-RK9rmBqk7sQOJMIBLMTP_lj_QlJ5fUNf2yjsF_OwXtz28q8IMp4abOwfUr1x_zprHTPPiQ1sqdSQ6svj__XFMZXKDCAQ5tg7qUxDMybiGFSaPhY3btMchoPVwiL4Cm6_IQNj:18091:31)
+            //     at unigraph_wasm.wasm.__wbg_new_8a6f238a6ece86ea externref shim (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[5223]:0x2eb595)
+            //     at unigraph_wasm.wasm.console_error_panic_hook::hook::hff5145660ffa5a5b (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[1997]:0x25e02e)
+            //     at unigraph_wasm.wasm.core::ops::function::Fn::call::ha961929f72d9db20 (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[6149]:0x2eeaf9)
+            //     at unigraph_wasm.wasm.std::panicking::rust_panic_with_hook::h645afae5b52932f1 (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[3102]:0x2b7cf8)
+            //     at unigraph_wasm.wasm.std::panicking::begin_panic_handler::{{closure}}::h46b0698008fa5278 (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[3531]:0x2cd088)
+            //     at unigraph_wasm.wasm.std::sys::backtrace::__rust_end_short_backtrace::h13efa606809930a8 (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[6048]:0x2ee731)
+            //     at unigraph_wasm.wasm.__rustc[8abf7dcf45103d39]::rust_begin_unwind (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[4739]:0x2e7aa4)
+            //     at unigraph_wasm.wasm.core::panicking::panic_fmt::h00d2fd22445b48f4 (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[4740]:0x2e7ad0)
+            //     at unigraph_wasm.wasm.alloc::raw_vec::capacity_overflow::h8995664c64bcb3fb (wasm://wasm/unigraph_wasm.wasm-010137f2:wasm-function[4602]:0x2e5f71)
+            //
+            //
+            // This happens rarely and will need to be debugged.
+            // My guess there's some potential cycle or something. cause if BFS succeeds i don't see
+            // how the path can exceed the number of BFS levels
             path.push(current);
             current = parent;
         }
